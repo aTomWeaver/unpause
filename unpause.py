e47271b7de3fce499181e71fbf37bee06e0ea380
aTomWeaver <atomweavermail@gmail.com>
2024-10-24 07:27:47 -0400

    add cli commands

--- unpause.py
+++ unpause.py
@@ -4,7 +4,57 @@ import subprocess
 from sys import argv
 
 
-# CLI Stuff
+def main():
+    if len(argv) < 2:
+        print("no args")
+        return
+    else:
+        args = argv[1:]
+    if not args[0] in CMDS:
+        print("not in commands")
+    else:
+        CMDS[args[0]]()
+
+
+def list_projects():
+    print("listing!")
+
+
+def add_project():
+    print("adding project!")
+
+
+def remove_project():
+    print("removing")
+
+
+def update_project():
+    print("update")
+
+
+def edit_project():
+    print("edit")
+
+
+def init_project():
+    print("init")
+
+
+def set_home():
+    print("setting home")
+
+
+CMDS = {
+        "list": list_projects,
+        "add": add_project,
+        "remove": remove_project,
+        "update": update_project,
+        "edit": edit_project,
+        "init": init_project,
+        "set-home": set_home,
+        }
+
+
 COLORS = {
         "header": '\033[95m',
         "blue": '\033[94m',
@@ -19,4 +69,4 @@ COLORS = {
 
 
 if __name__ == "__main__":
-    print(f"{COLORS["header"]}yo")
+    main()
