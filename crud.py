9cf7a6242ab997ec40f5672f68cb4b9d279992d6
aTomWeaver <atomweavermail@gmail.com>
2024-10-27 09:13:03 -0400

    make project dir and write script file functions

---
+++ crud.py
@@ -0,0 +1,35 @@
+import os
+import pickle
+
+
+PROJ_INDEX = "data/data.pickle"
+PROJ_DIR = "projects"
+
+
+def read_projects_data() -> dict:
+    '''Read project data and return it as a dict.'''
+    with open(PROJ_INDEX, 'rb') as file:
+        projects_data = pickle.load(file)
+    return projects_data
+
+
+def write_projects_data(projects_data: dict):
+    '''Write project data dict to pickle file.'''
+    with open(PROJ_INDEX, "wb") as file:
+        pickle.dump(projects_data, file)
+
+
+def make_project_dir(name):
+    '''Make a new directory for a project and return the realpath.'''
+    dir_path = os.path.join(PROJ_DIR, name)
+    # if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+    return os.path.realpath(dir_path)
+
+
+def write_script_file(script: str, path: str):
+    '''Write a script file given the script template and path.'''
+    with open(path, 'w+') as file:
+        file.write(script)
+
+
